3e3b6f8d1306e9508c47118084f1733bff3ec25c
Hareesh <119724521+Harish-lvrk@users.noreply.github.com>
2025-08-15 06:49:27 +0000

    rendering the dynamic data using jinja tags

--- app.py
+++ app.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    'title': "Data Analyst",
+    'location': "Bengulur, India",
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    "id": 2,
+    'title': "Agent Work",
+    'location': "Vishakapatnam, India",
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    "id": 3,
+    'title': "ML Engineer",
+    'location': "Remote",
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    "id": 4,
+    'title': "Full Stakc Developer",
+    'location': "Hyderabad, India",
+    'salary': '$. 8,00,000'
+  },
+  {
+    "id": 5,
+    'title': "Backend Engineer",
+    'location': "San Fransisco",
+    
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs = JOBS ,company_name = "Stark")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
