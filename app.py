0db61223dd02d36443f150c81d95eb9ca5d616f4
Hareesh <119724521+Harish-lvrk@users.noreply.github.com>
2025-08-16 15:48:14 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template , jsonify
-from database import get_jobs
+from database import get_jobs , get_specific_job
 app = Flask(__name__)
 
 
@@ -12,11 +12,15 @@ def hello_world():
 @app.route('/api/jobs')
 def list_jobs():
   jobs_from_db = get_jobs()
+  return jsonify(jobs_from_db)
+  
+@app.route('/job/<id>')
+def show_jobs(id):# this is what the rest api is with url getting the json object
+  job = get_specific_job(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',job = job)
 
-  # Convert the list of RowMapping objects into a list of dictionaries
-  jobs_list = [dict(row) for row in jobs_from_db]
-
-  return jsonify(jobs_list)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 
 from sqlalchemy import create_engine, text 
 import os
+
 # SQLAlchemy engine using PyMySQL + SSL
 engine = create_engine(
     os.environ['DB_CONNECTION_STRING'],
@@ -17,8 +18,24 @@ def get_jobs():
     with engine.connect() as connection:
         result = connection.execute(text("SELECT * FROM jobs"))
         jobs = result.mappings().all()   # here jobs is a list of dictionaries
-        return jobs
+        
+         # Convert the list of RowMapping objects into a list of dictionaries
+        jobs_list = [dict(row) for row in jobs]
+        return jobs_list
 
-            
+def get_specific_job(id):
+   
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val": id}  # Pass parameters as a dictionary
+        )
+        row = result.fetchone() # Fetches the first matching row
+
+        if row is None:
+            return None # Or handle as you see fit (e.g., raise an error)
+        else:
+            return dict(row._mapping) # Converts the RowMapping to a dictionary
+
         
    
