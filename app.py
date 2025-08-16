18be5d8c81cb335e49a78d8b71c5dc61c6631ef7
Hareesh <119724521+Harish-lvrk@users.noreply.github.com>
2025-08-16 17:34:12 +0000

    we stored the application data into the database and other work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template , jsonify
-from database import get_jobs , get_specific_job
+from flask import Flask, render_template , jsonify , request
+from database import get_jobs , get_specific_job , add_application_to_db
 app = Flask(__name__)
 
 
@@ -20,6 +20,16 @@ def show_jobs(id):# this is what the rest api is with url getting the json objec
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html',job = job)
+
+@app.route('/job/<id>/apply' , methods = ['post'])
+def apply_to_job(id):
+  data = request.form
+  # save the details in the database
+  #sending the acknowledgements
+  #
+  job = get_specific_job(id)
+  add_application_to_db(id, data)
+  return render_template('application_submit.html', application = data ,job = job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -38,4 +38,25 @@ def get_specific_job(id):
             return dict(row._mapping) # Converts the RowMapping to a dictionary
 
         
-   
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text(
+            """
+            INSERT INTO applications 
+            (job_id, full_name, email, linkdin_url, education, work_experiance, resume_url)
+            VALUES (:job_id, :full_name, :email, :linkdin_url, :education, :work_experiance, :resume_url)
+            """
+        )
+        conn.execute(
+            query,
+            {
+                "job_id": job_id,
+                "full_name": data["full_name"],
+                "email": data["email"],
+                "linkdin_url": data["linkdin_url"],
+                "education": data["education"],
+                "work_experiance": data["work_experiance"],
+                "resume_url": data["resume_url"]
+            }
+        )
+        conn.commit()
